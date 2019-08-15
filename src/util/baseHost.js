8044ce2b66e5ec8a0a5d710d1ea8d5328ebafa58
yuanyao <kopanya@dingtalk.com>
2019-08-15 10:31:19 +0800

    feat: 注释测试代码

--- src/util/baseHost.js
+++ src/util/baseHost.js
@@ -10,6 +10,6 @@ if (host.indexOf('daily-xt-crmadmin') >= 0) {
   baseHost = 'https://youxuan-crm-api.hzxituan.com';
 }
 
-baseHost = 'http://192.168.10.49:8080';
+// baseHost = 'http://192.168.10.49:8080';
 
 export default baseHost;
