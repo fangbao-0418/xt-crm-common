ce197d5a44693ae78df82e69135daa8e4c9ebd23
wucl <1445860318@qq.com>
2019-08-16 15:22:38 +0800

    fix: 退货退款、仅换货才有退货信息

--- src/pages/order/refund/components/check-detail/index.js
+++ src/pages/order/refund/components/check-detail/index.js
@@ -8,7 +8,7 @@ function CheckDetail({ checkVO = {}, orderServerVO = {}, checkType }) {
   return (
     <>
       <CustomerServiceReview checkVO={checkVO} orderServerVO={orderServerVO} />
-      <ReturnInformation checkVO={checkVO} />
+      {orderServerVO.refundType !== '20' && <ReturnInformation checkVO={checkVO} />}
       {/* 退货退款、仅换货 */}
       {orderServerVO.refundType === '10' && <RefundInformation checkVO={checkVO} orderServerVO={orderServerVO} checkType={checkType} />}
       {orderServerVO.refundType === '30' && <DeliveryInformation checkVO={checkVO} checkType={checkType} refundType={orderServerVO.refundType} />}
