a460f038680b74d478f6e7a0133d269b9f0e9111
wucl <1445860318@qq.com>
2019-08-17 21:03:51 +0800

    feat: 暂存

--- src/pages/activity/coupon/list/bulk-issuing/index.js
+++ src/pages/activity/coupon/list/bulk-issuing/index.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { formItemLayout } from '@/config';
-import { DatePicker, Card, Form, Checkbox, Input, Button, Radio } from 'antd';
-const plainOptions = ['普通用户', '团长', '体验团长', '星级团长', '社区管理员', '城市合伙人'];
+import { formItemLayout, formButtonLayout } from '@/config';
+import { DatePicker, Row, Col, Card, Form, Checkbox, Input, Button, Radio } from 'antd';
 
 // 批量发券
 function BulkIssuing({ form: { getFieldDecorator } }) {
   console.log(getFieldDecorator)
-  const [radioKey, setRadioKey] = useState(1)
+  const [targetUsersKey, setTargetUsersKey] = useState(1)
+  const [sendingTimeKey, setSendingTimeKey] = useState(1)
   const radioStyle = {
     display: 'block',
     height: '30px',
@@ -14,35 +14,49 @@ function BulkIssuing({ form: { getFieldDecorator } }) {
   }
   return (
     <Card>
-      {/* <Row>
-        <Col offset={5}>
-          <h2>基本信息</h2>
-        </Col>
-      </Row> */}
       <Form {...formItemLayout}>
         <Form.Item label="优惠券名称">中秋国庆发券</Form.Item>
         <Form.Item label="目标用户">
-          <Radio.Group onChange={event => setRadioKey(event.target.value)} value={radioKey}>
+          <Radio.Group onChange={event => setTargetUsersKey(event.target.value)} value={targetUsersKey}>
             <Radio style={radioStyle} value={1}>全部用户</Radio>
-            <Radio style={radioStyle} value={2}>按用户等级</Radio>
-            <div>
-              <Checkbox.Group options={plainOptions} />
-            </div>
+            <Radio style={radioStyle} value={2}>按用户等级<Button type="link">选择等级</Button></Radio>
+            {targetUsersKey === 2 &&　<Checkbox.Group style={{ width: '100%' }}>
+              <Row>
+                <Col span={8}>
+                  <Checkbox value="普通用户">普通用户</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="团长">团长</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="体验团长">体验团长</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="星级团长">星级团长</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="社区管理员">社区管理员</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="城市合伙人">城市合伙人</Checkbox>
+                </Col>
+              </Row>
+            </Checkbox.Group>}
             <Radio style={radioStyle} value={3}>指定用户</Radio>
-            <Input.TextArea rows={4} />
+            <Input.TextArea style={{width: '528px'}} rows={4} placeholder="输入用户手机号，以半角逗号隔开，例13928387247,15619237922"/>
             <Button type="link">上传excel</Button>
           </Radio.Group>
         </Form.Item>
         <Form.Item label="发送时间">
-          <Radio.Group onChange={event => setRadioKey(event.target.value)} value={radioKey}>
-            <Radio style={radioStyle} value={1}>立即发送</Radio>
-            <Radio style={radioStyle} value={2}>定时发送</Radio>
+          <Radio.Group onChange={event => setSendingTimeKey(event.target.value)} value={sendingTimeKey}>
+            <Radio style={radioStyle} value={3}>立即发送</Radio>
+            <Radio style={radioStyle} value={4}>定时发送</Radio>
             <div>
-              选择时间：<DatePicker />
+              选择时间：<DatePicker format="YYYY-MM-DD HH:mm:ss"/>
             </div>
           </Radio.Group>
         </Form.Item>
-        <Form.Item>
+        <Form.Item {...formButtonLayout}>
           <Button type="primary">保存</Button>
           <Button>取消</Button>
         </Form.Item>
