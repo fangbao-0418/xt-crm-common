911174d40e4f81106c39bf418d559a7c8cc6f5a4
wucl <1445860318@qq.com>
2019-08-20 23:05:53 +0800

    fix: 售后单订单详情已完成状态没有显示退款金额和退运费字段

--- src/pages/order/refund/components/check-detail/index.js
+++ src/pages/order/refund/components/check-detail/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import CustomerServiceReview from '../customer-service-review';
+import { Card, Row, Col } from 'antd';
+import { formatMoney, joinFilterEmpty } from '@/pages/helper'
+import refundType from '@/enum/refundType';
 import ReturnInformation from '../return-information';
 import DeliveryInformation from '../delivery-information';
 import RefundInformation from '../refund-information';
@@ -7,7 +9,20 @@ import RefundInformation from '../refund-information';
 function CheckDetail({ checkVO = {}, orderServerVO = {}, checkType }) {
   return (
     <>
-      <CustomerServiceReview checkVO={checkVO} orderServerVO={orderServerVO} />
+      <Card title="审核信息">
+        <Row>
+          <Col>审核意见：{checkVO.firstRefundStatusStr || checkVO.refundStatusStr}</Col>
+          <Col>退款类型：{refundType.getValue(checkVO.refundType)}</Col>
+          {checkVO.refundType != 30 && (
+            <>
+              <Col>退款金额：￥{formatMoney(checkVO.refundAmount)}</Col>
+              <Col>退运费：￥{formatMoney(checkVO.freight)}</Col>
+            </>
+          )}
+          <Col>说明：{checkVO.firstServerDescribe || checkVO.serverDescribe}</Col>
+          {orderServerVO.refundType != '20' && <Col>退货信息：{joinFilterEmpty([checkVO.returnContact, checkVO.returnPhone, checkVO.returnAddress])}</Col>}
+        </Row>
+      </Card>
       {orderServerVO.refundType !== '20' && <ReturnInformation checkVO={checkVO} />}
       {/* 退货退款、仅换货 */}
       {orderServerVO.refundType === '10' && <RefundInformation checkVO={checkVO} orderServerVO={orderServerVO} checkType={checkType} />}
--- src/pages/order/refund/components/customer-service-review.js
+++ src/pages/order/refund/components/customer-service-review.js
@@ -8,8 +8,8 @@ function CustomerServiceReview({ checkVO = {}, orderServerVO = {} }) {
       <Row>
         <Col>审核意见：{ checkVO.firstRefundStatusStr || checkVO.refundStatusStr}</Col>
         <Col>退款类型：{refundType.getValue(checkVO.refundType)}</Col>
-        {checkVO.refundType === '20' && <Col>退款金额：￥{formatMoney(checkVO.refundAmount)}</Col>}
-        {/* 仅退款、退货退款 */ checkVO.refundType === '20' && orderServerVO.refundType !== '30' && <Col>退运费：￥{formatMoney(checkVO.freight)}</Col>}
+        {checkVO.refundType == '20' && <Col>退款金额：￥{formatMoney(checkVO.refundAmount)}</Col>}
+        {/* 仅退款、退货退款 */ checkVO.refundType == '20' && orderServerVO.refundType !== '30' && <Col>退运费：￥{formatMoney(checkVO.freight)}</Col>}
         <Col>说明：{checkVO.firstServerDescribe || checkVO.serverDescribe}</Col>
         {/* 退货退款、仅换货 */ orderServerVO.refundType !== '20' && <Col>退货信息：{joinFilterEmpty([checkVO.returnContact, checkVO.returnPhone, checkVO.returnAddress])}</Col>}
       </Row>
