20ba0cdd6ab081179d154ee6df3128a0b0a49bc8
wucl <1445860318@qq.com>
2019-09-27 21:51:07 +0800

    feat: 物流公司名称编码放入配置中

--- src/components/express-company-select/index.js
+++ src/components/express-company-select/index.js
@@ -1,34 +1,8 @@
 import React, { Component } from 'react';
+import { ExpressCompanyOptions } from '@/config'
 import { Select } from 'antd';
 
 const { Option } = Select;
-
-// EMS("ems", "EMS"),
-// SHUNFENG("shunfeng", "顺丰"),
-// SHENTONG("shentong", "申通"),
-// YUANTONG("yuantong", "圆通"),
-// ZHONGTONG("zhongtong", "中通"),
-// HUITONGKUAIDI("huitongkuaidi", "汇通"),
-// YUNDA("yunda", "韵达"),
-// GUOTONGKUAIDI("guotongkuaidi", "国通"),
-// DEBANGWULIU("debangwuliu", "德邦"),
-// JD("jd", "京东"),
-
-export const ExpressCompanyOptions = {
-  ems: 'EMS',
-  shunfeng: '顺丰',
-  shentong: '申通',
-  yuantong: '圆通',
-  zhongtong: '中通',
-  huitongkuaidi: '汇通',
-  yunda: '韵达',
-  guotongkuaidi: '国通',
-  debangwuliu: '德邦',
-  jd: '京东',
-  tiantian: '天天快递',
-  youzhengbk: "邮政标准快递",
-  youzhengguonei: "邮政快递包裹"
-};
 
 const valueArray = Object.keys(ExpressCompanyOptions);
 
