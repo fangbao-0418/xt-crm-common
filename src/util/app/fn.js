e4ad2951648466d5a04e9dec648ef01b2935b740
方宝 <1197036595@qq.com>
2019-10-27 18:55:48 +0800

    feat: getH5Origin链接添加版本 Update fn.js

--- src/util/app/fn.js
+++ src/util/app/fn.js
@@ -1,10 +1,11 @@
 export function getH5Origin () {
   let origin = 'https://daily-myouxuan.hzxituan.com/'
+  const nowTime = new Date().getTime()
   const host = window.location.host;
   if (host.indexOf('daily-xt-crmadmin') >= 0) {
     origin = 'https://daily-myouxuan.hzxituan.com/';
   } else if (host.indexOf('pre-xt-crmadmin') >= 0) {
-    origin = 'https://pre-xt-myouxuan.hzxituan.com/pre/index.html';
+    origin = 'https://pre-xt-myouxuan.hzxituan.com/pre/';
   } else if (host.indexOf('test-crmadmin') >= 0) {
     origin = 'https://testing.hzxituan.com/';
   } else if (host.indexOf('test2-crmadmin') >= 0) {
@@ -13,5 +14,5 @@ export function getH5Origin () {
     /** 正式 */
     origin = 'https://myouxuan.hzxituan.com/';
   }
-  return origin
+  return origin + `index.html?v=${nowTime}/`
 }
