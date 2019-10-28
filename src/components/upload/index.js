77707910983b4649313e86ce4bf5df02e613e5df
yangyl <438933868@qq.com>
2019-10-28 17:39:09 +0800

    fix: 添加图片大小多种限制

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -104,13 +104,16 @@ class UploadView extends Component {
       message.error(`请上传小于${size * 1000}kB的文件`);
       return Promise.reject()
     }
-    //pxSize: {width:100, height:100} 限制图片上传px大小
-    if (pxSize) {
+    //pxSize: [{width:100, height:100}] 限制图片上传px大小
+    if (pxSize && pxSize.length) {
       const imgSize = await this.getImgSize(file) || {width:0, height:0}
-      console.log('imgSize:pxSize', imgSize, pxSize)
-      if (!(imgSize.width ==  pxSize.width && imgSize.height == pxSize.height)) {
-        message.error(`图片尺寸不正确`);
-        return Promise.reject()
+      for (let index = 0; index < pxSize.length; index++) {
+        const element = pxSize[index];
+        console.log('imgSize:pxSize', imgSize, element)
+        if (!(imgSize.width ==  element.width && imgSize.height == element.height)) {
+          message.error(`图片尺寸不正确`);
+          return Promise.reject()
+        }
       }
     }
     return Promise.resolve(file)
