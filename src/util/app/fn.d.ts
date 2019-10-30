e1847c3353fd259a8a315d0fed537ba2603111b9
fangbao <fangbao@i-counting.cn>
2019-10-30 10:47:55 +0800

    feat: 合并冲突

--- src/util/app/fn.d.ts
+++ src/util/app/fn.d.ts
@@ -1,6 +1,10 @@
 interface FnProps {
   /** 获取h5域名 */
   getH5Origin: () => string
+  /** 格式化日期 */
+  formatDate: (date: string | number, format?: string) => string
+  /** 格式化金额 */
+  formatMoney: (money: any) => string
   /** 处理loading 默认参数loadind=end */
   handleLoading: (status?: 'start' | 'end') => void
 }
