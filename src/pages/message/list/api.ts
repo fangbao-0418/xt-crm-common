b20c60ae9f981855f64df5bd79da0454f2689ab5
wucl <1445860318@qq.com>
2019-11-11 21:25:35 +0800

    feat: 修改api地址

--- src/pages/message/list/api.ts
+++ src/pages/message/list/api.ts
@@ -4,24 +4,24 @@ export const getList = (payload: any) => {
   // /msg/group/queryList
   // http://mock-ued.hzxituan.com/mock/5da1a097b62ce300168bb5d7/crm/demo/list#!method=get
   console.log(payload, '---------  getList ')
-  return newPost('http://192.168.20.16:39003/msg/group/queryList', payload)
+  return newPost('https://test01center-bi.hzxituan.com/msg/group/queryList', payload)
 }
 
 export const fetchDetail = (id: any) => {
-  return newPost(`http://192.168.20.16:39003/msg/group/selectById/${id}`)
+  return newPost(`https://test01center-bi.hzxituan.com/msg/group/selectById/${id}`)
 }
 
 export const saveMessage = (payload: Message.ItemProps) => {
-  return newPost(`http://192.168.20.16:39003/msg/group/save`, payload)
+  return newPost(`https://test01center-bi.hzxituan.com/msg/group/save`, payload)
 }
 
 export const deleteMessage = (id: any) => {
-  return del(`http://192.168.20.16:39003/msg/group/delete/${id}`)
+  return del(`https://test01center-bi.hzxituan.com/msg/group/delete/${id}`)
 }
 
 /** 取消任务发送 */
 export const cancelTaskSend = (id: any) => {
-  return newPost(`http://192.168.20.16:39003/msg/group/updateStatusById`, {
+  return newPost(`https://test01center-bi.hzxituan.com/msg/group/updateStatusById`, {
     pushType: 4,
     jobId: id
   })
