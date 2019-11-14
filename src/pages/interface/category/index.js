7e400b2d65cd71f9d4a1612c839441ea806323ca
cuizhu <jiguang.hjg@dtwave-inc.com>
2019-11-14 21:03:51 +0800

    feat: 类目活动筛选增加搜索条件

--- src/pages/interface/category/index.js
+++ src/pages/interface/category/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { Card, Row, Col, Form, Checkbox, Button, Switch, Spin, Input, Icon, Modal, Table, Tree, message } from 'antd';
+import { Card, Row, Col, DatePicker, Form, Checkbox, Button, Switch, Spin, Input, Icon, Modal, Table, Tree, message, Select } from 'antd';
 import DateFns from 'date-fns';
 import { initImgList } from '@/util/utils';
 import { getPromotionList } from '../../activity/api';
 import { getFrontCategorys, getCategory, delCategory, saveFrontCategory, updateFrontCategory } from './api';
 import Ctree from './tree';
+import moment from 'moment';
 import SecondaryCategory from './secondaryCategory'
 import activityType from '../../../enum/activityType'
 import _ from 'lodash';
 import './category.scss';
 
+const { Option } = Select;
 const FormItem = Form.Item;
+const { RangePicker } = DatePicker;
+
 const formLayout = {
   labelCol: {
     xs: { span: 24 },
@@ -60,7 +64,8 @@ class InterFaceCategory extends Component {
     currId: 0,
     productCategoryVOS: [],
     checkData: [],
-    secondStatus: false
+    secondStatus: false,
+    activityParams: {}
   }
 
   selectedRows = []
@@ -285,20 +290,21 @@ class InterFaceCategory extends Component {
 
   }
   getPromotionList = params => {
-    params = params || {}
-    const { modalPage } = this.state;
+    const { activityParams, modalPage } = this.state;
+
+    const nowParams = params || activityParams;
     // page.current += 1;
     getPromotionList({
-      page: modalPage.current,
+      ...nowParams,
+      page: params ? 1 : modalPage.current,
       pageSize: modalPage.pageSize,
-      ...params
     }).then(res => {
       modalPage.total = res.total;
 
       this.setState({
         actList: res.records,
-        //  selectedRowKeys: [],
-        modalPage,
+        modalPage: { ...modalPage, current: params ? 1 : modalPage.current},
+        activityParams: nowParams
       });
     });
   };
@@ -507,30 +513,17 @@ class InterFaceCategory extends Component {
             </Form.Item>
           </Form>
         </Card>
-        <Modal
-          title="选择活动"
-          visible={visible1}
-          width={1000}
-          onCancel={this.handleCancelModal}
-          onOk={this.handleOkModal}
-        >
-          {/* <Input.Search
-            placeholder="请输入需要搜索的活动"
-            style={{ marginBottom: 10 }}
-            onSearch={this.handleSearchModal}
-          /> */}
-          <Table
-            rowSelection={{
-              selectedRowKeys: selectedRowKeys,
-              onChange: this.handlenChanageSelectio,
-            }}
-            columns={actColumns()}
-            dataSource={actList}
-            pagination={modalPage}
-            onChange={this.handleTabChangeModal}
-            rowKey={record => record.id}
-          />
-        </Modal>
+        <GetActivityModal
+          handleCancelModal={this.handleCancelModal}
+          handleOkModal={this.handleOkModal}
+          actList={actList}
+          selectedRowKeys={selectedRowKeys}
+          modalPage={modalPage}
+          visible1={visible1}
+          handleTabChangeModal={this.handleTabChangeModal}
+          handlenChanageSelectio={this.handlenChanageSelectio}
+          getPromotionList={this.getPromotionList}
+        />
         <Modal
           title="选择类目"
           wrapClassName="intf-cat-tree-box"
@@ -592,6 +585,123 @@ const actColumns = (data = []) => {
     }
   ]
 };
+
+class GetActivity extends Component {
+
+  handleSearch = () => {
+    const {
+      form: { validateFields },
+    } = this.props;
+    validateFields((err, vals) => {
+      if (!err) {
+        let params = {
+          ...vals,
+          startTime: vals.time && vals.time[0] && +new Date(vals.time[0]),
+          endTime: vals.time && vals.time[1] && +new Date(vals.time[1]),
+          page: 1,
+          pageSize: 20,
+        };
+        params = _.omitBy(params, (val, key) => {
+          return key === 'time' || _.isNil(val) || val === '';
+        });
+        this.props.getPromotionList(params);
+      }
+    });
+  };
+  render() {
+    const { 
+      handleCancelModal, handleOkModal, 
+      actList, selectedRowKeys, modalPage, 
+      visible1, handleTabChangeModal, 
+      handlenChanageSelectio, form
+    } = this.props;
+    const { getFieldDecorator } = form;
+    return <Modal
+        title="选择活动"
+        visible={visible1}
+        width={1000}
+        onCancel={handleCancelModal}
+        onOk={handleOkModal}
+      >
+        <Form layout="inline" style={{marginBottom: '20px'}}>
+            <FormItem label="活动名称">
+              {getFieldDecorator('name', {
+              })(<Input placeholder="请输入活动名称" style={{ width: 180 }} />)}
+            </FormItem>
+            <FormItem label="活动ID">
+              {getFieldDecorator('promotionId', {
+              })(<Input placeholder="请输入活动ID" style={{ width: 180 }} />)}
+            </FormItem>
+            <FormItem label="商品名称">
+              {getFieldDecorator('productName')(
+                <Input placeholder="请输入商品名称" style={{ width: 180 }} />,
+              )}
+            </FormItem>
+            <FormItem label="商品ID">
+              {getFieldDecorator('productId')(
+                <Input placeholder="请输入商品ID" style={{ width: 180 }} />,
+              )}
+            </FormItem>
+            <FormItem label="活动类型">
+              {getFieldDecorator('type', {
+              })(
+                <Select placeholder="请选择活动类型" style={{ width: 180 }}>
+                  <Option value="">全部</Option>
+                  {activityType.getArray().map((val, i) => (
+                    <Option value={val.key} key={i}>
+                      {val.val}
+                    </Option>
+                  ))}
+                </Select>,
+              )}
+            </FormItem>
+            <FormItem label="活动状态">
+              {getFieldDecorator('status', {
+              })(
+                <Select placeholder="请选择活动类型" style={{ width: 180 }}>
+                  <Option value="">全部</Option>
+                  <Option value="0">关闭</Option>
+                  <Option value="1">开启</Option>
+                </Select>,
+              )}
+            </FormItem>
+            <FormItem label="有效时间">
+              {getFieldDecorator('time', {
+              })(
+                <RangePicker
+                  style={{ width: 430 }}
+                  format="YYYY-MM-DD HH:mm"
+                  showTime={{
+                    defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('23:59:59', 'HH:mm:ss')],
+                  }}
+                />,
+              )}
+            </FormItem>
+            <div style={{ textAlign: 'right', marginTop: 8 }}>
+              <Button type="primary" onClick={this.handleSearch}>
+                查询
+              </Button>
+              <Button style={{ marginLeft: 10 }} onClick={this.resetSearch}>
+                重置
+              </Button>
+            </div>
+          </Form>
+        <Table
+          rowSelection={{
+            selectedRowKeys: selectedRowKeys,
+            onChange: handlenChanageSelectio,
+          }}
+          columns={actColumns()}
+          dataSource={actList}
+          pagination={modalPage}
+          onChange={handleTabChangeModal}
+          rowKey={record => record.id}
+        />
+      </Modal>
+  }
+}
+
+const GetActivityModal = Form.create()(GetActivity)
 
 
 export default Form.create()(InterFaceCategory);
