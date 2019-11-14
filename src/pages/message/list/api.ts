928b35230dff6c9100f819ffae9d524b544a43e7
fangbao <fangbao@hzxituan.com>
2019-11-14 18:13:18 +0800

    feat: 修改消息服务

--- src/pages/message/list/api.ts
+++ src/pages/message/list/api.ts
@@ -4,24 +4,24 @@ export const getList = (payload: any) => {
   // /msg/group/queryList
   // http://mock-ued.hzxituan.com/mock/5da1a097b62ce300168bb5d7/crm/demo/list#!method=get
   console.log(payload, '---------  getList ')
-  return newPost('https://test01center-bi.hzxituan.com/msg/group/queryList', payload)
+  return newPost('/msg/group/queryList', payload)
 }
 
 export const fetchDetail = (id: any) => {
-  return newPost(`https://test01center-bi.hzxituan.com/msg/group/selectById/${id}`)
+  return newPost(`/msg/group/selectById/${id}`)
 }
 
 export const saveMessage = (payload: Message.ItemProps) => {
-  return newPost(`https://test01center-bi.hzxituan.com/msg/group/save`, payload)
+  return newPost(`/msg/group/save`, payload)
 }
 
 export const deleteMessage = (id: any) => {
-  return del(`https://test01center-bi.hzxituan.com/msg/group/delete/${id}`)
+  return del(`/msg/group/delete/${id}`)
 }
 
 /** 取消任务发送 */
 export const cancelTaskSend = (id: any) => {
-  return newPost(`https://test01center-bi.hzxituan.com/msg/group/updateStatusById`, {
+  return newPost(`/msg/group/updateStatusById`, {
     pushType: 4,
     jobId: id
   })
