61799af43a7bb64fa3cb97b12265cb4ddac031e9
wucl <1445860318@qq.com>
2019-11-18 16:55:57 +0800

    调整仓库中和商品池的位置

--- src/pages/goods/view.js
+++ src/pages/goods/view.js
@@ -21,8 +21,8 @@ const goods = props => {
       <Card>
         <Tabs defaultActiveKey={initialStatus} onChange={callback}>
           <TabPane tab="出售中" key="0" />
-          <TabPane tab="商品池" key="2" />
           <TabPane tab="仓库中" key="1" />
+          <TabPane tab="商品池" key="2" />
         </Tabs>
         <List key={status} status={status} />
       </Card>
