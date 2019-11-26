44070dcc1ce72a20a44a45dbdd6591fde4569aca
wucl <1445860318@qq.com>
2019-11-26 21:23:15 +0800

    设置最小值1

--- src/pages/setting/my/detail.tsx
+++ src/pages/setting/my/detail.tsx
@@ -230,6 +230,7 @@ class Main extends React.Component<any, State> {
                   controlProps={{
                     placeholder: '排序越大越靠前',
                     precision: 0,
+                    min: 1,
                     style: {
                       width: 200
                     }
