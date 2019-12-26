1196e82e72b01e2c557c020d165718ae7ad23b68
方宝 <fangbao@hzxituan.com>
2019-12-26 15:38:21 +0800

    Update config.ts

--- src/util/app/config.ts
+++ src/util/app/config.ts
@@ -3,8 +3,9 @@ import { baseHost, env as apiEnv } from '../baseHost'
 /** 后端环境接口映射 */
 export const serverMapper: any = {
   ulive: {
-    local: 'https://dailylive-center.hzxituan.com',
     dev: 'https://test01live-center.hzxituan.com',
+    test1: 'https://test01live-center.hzxituan.com',
+    test2: 'https://test01live-center.hzxituan.com',
     pre: 'https://test01live-center.hzxituan.com',
     prod: 'http://192.168.4.117:1008'
   },
