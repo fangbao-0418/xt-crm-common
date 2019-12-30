31647f1e989f16f61523957a348df4fd560991b6
fangbao <fangbao@hzxituan.com>
2019-12-30 11:42:30 +0800

    fix: 直播测试

--- src/pages/ulive/studio/components/Video.tsx
+++ src/pages/ulive/studio/components/Video.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TcPlayer } from '@/util/TcPlayer-module-2.3.2'
+// import { TcPlayer } from '@/util/TcPlayer-module-2.3.2'
 interface Props {
   detail: UliveStudio.ItemProps
   hide?: () => void
