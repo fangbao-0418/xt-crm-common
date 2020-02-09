669b2db8bd78da33249c5cb39d3489f871752f78
wucl <1445860318@qq.com>
2020-02-09 15:45:45 +0800

    feat: 不使用name判断，防止出现错误

--- src/components/readonly-component/index.tsx
+++ src/components/readonly-component/index.tsx
@@ -13,11 +13,10 @@ class ReadOnlyComponent extends React.Component<ReadOnlyComponentProps, any> {
     const { value, children, readOnly } = this.props;
     const otherProps = omit(this.props, ['readOnly', 'children']);
     const _children = children as React.ReactElement;
-    const name = (_children.type as any).name;
     let text = value;
     // RangePicker组件
-    if (name === 'PickerWrapper' && Array.isArray(value) && value.length === 2) {
-      text = (value as Moment[]).map(m => m.format('YYYY-MM-DD HH:mm:ss')).join('~');
+    if (Array.isArray(value) && value.length === 2) {
+      text = (value as Moment[]).map(m => m.format && m.format('YYYY-MM-DD HH:mm:ss')).join('~');
     }
     return readOnly ? <span>{text}</span> : React.cloneElement(_children, otherProps);
   }
