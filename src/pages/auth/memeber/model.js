34547ea5b48899e1b24ee6f04231985ce00565f8
lvhuan <lvhuan@hzxituan.com>
2020-02-14 10:28:03 +0800

    fix: 修复bug-2209：操作权限管理后再对新添加成功的账号进行权限管理时，会带入上一次操作的角色信息

--- src/pages/auth/memeber/model.js
+++ src/pages/auth/memeber/model.js
@@ -79,7 +79,7 @@ export default {
       const res = await api.getUserRoles({
         id: payload.id
       });
-      if (res && res.length) {
+      if (res) {
         dispatch['auth.member'].saveDefault({
           selectedRowKeys: res.map(item => item.id)
         });
