a6e1484a2fa2a2f5b20e38d71db73576113e992e
fangbao <fangbao@i-counting.cn>
2020-02-19 11:21:03 +0800

    feat: 对账单明细金额样式优化

--- src/pages/merchant-accounts/checking/Detail.tsx
+++ src/pages/merchant-accounts/checking/Detail.tsx
@@ -122,9 +122,11 @@ class Main extends React.Component<Props, State> {
         </div>
         <div className={styles['detail-header2']}>
           <div>
-            <div>收入：<span>{query.incomeMoney || '0.00'}</span>元</div>
-            <div>支出：<span>{query.disburseMoney || '0.00'}</span>元</div>
-            <div>本期对对账单总额：<span>{query.settlementMoney || '0.00'}</span>元</div>
+            <div>收入：<span className='success'>{query.incomeMoney || '0.00'}</span>元</div>
+            <div>支出：<span className='error'>{query.disburseMoney || '0.00'}</span>元</div>
+            <div>本期对对账单总额：
+              <span className={Number(query.settlementMoney) > 0 ? 'success' : 'error'}>{query.settlementMoney || '0.00'}</span>元
+            </div>
           </div>
           <div>
             <Auth code='adjustment:procurement_audit'>
