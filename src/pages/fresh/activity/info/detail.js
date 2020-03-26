7da8921778b05e4b99ce5d79969ffe453f64c904
fangbao <fangbao@hzxituan.com>
2020-03-26 22:34:59 +0800

    feat: 修改活动库存

--- src/pages/fresh/activity/info/detail.js
+++ src/pages/fresh/activity/info/detail.js
@@ -264,19 +264,19 @@ class ActivityDetail extends React.Component {
             value: text,
             onChange: this.handleChangeValue('inventory', index)
           }
-          if (record.sellableQty) {
-            props.max = record.sellableQty
-          }
+          // if (record.sellableQty) {
+          //   props.max = record.sellableQty
+          // }
           return speedyInput('inventory', text, record, index, detailData.promotionSkuList, this.handleChangeValue)(<InputNumber {...props} />)
         },
       },
       {
-        title: '可用库存',
+        title: '商品可用库存',
         dataIndex: 'stock',
         render: (text, record, index) => {
           // 1.售后详情中 订单信息模块 需要添加订单类型的属性
           // 海淘商品可用库存需要读取保宏仓的可用库存数量，活动库存不可大于可用库存
-          return <span>{record.sellableQty || '无限制' }</span>
+          return <span>{record.sellableQty}</span>
         }
       },
       {
