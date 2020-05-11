b518997772bee18b7430c0575da70d8bb07605b9
biansaijun <biansaijun@hzxituan.com>
2020-05-11 16:56:19 +0800

    feat：运费模板详情 接口更改

--- src/pages/template/api.tsx
+++ src/pages/template/api.tsx
@@ -1,33 +1,33 @@
-import { newPost, get } from '@/util/fetch';
+import { newPost, get } from '@/util/fetch'
 
 /**
  * 创建运费模板
  * @param data
  */
-export function templateAdd(data: any) {
-    return newPost('/template/add', data)
+export function templateAdd (data: any) {
+  return newPost('/template/add', data)
 }
 
 /**
  * 运费模板列表分页
- * @param data 
+ * @param data
  */
-export function templatePage(data: any) {
-    return newPost('/template/page', data)
+export function templatePage (data: any) {
+  return newPost('/template/page', data)
 }
 
 /**
  * 查看单个运费模板
- * @param freightTemplateId 
+ * @param freightTemplateId
  */
-export function getDetail(freightTemplateId: number) {
-  return get(`/template/detail`, { freightTemplateId })
+export function getDetail (freightTemplateId: number) {
+  return get('/mcweb/product/template/detail', { freightTemplateId })
 }
 
 /**
  * 编辑运费模板
- * @param data 
+ * @param data
  */
-export function templateModify(data: any) {
+export function templateModify (data: any) {
   return newPost('/template/modify', data)
 }
