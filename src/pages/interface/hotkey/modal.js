620c4e7f15d3139450b987b8e48d8b26a0624ae5
biansaijun <biansaijun@hzxituan.com>
2020-05-25 15:58:49 +0800

    feat: 在售商品库存商品查询接口优化&热词配置删除接口接口优化

--- src/pages/interface/hotkey/modal.js
+++ src/pages/interface/hotkey/modal.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Input, Form, Modal, InputNumber, message } from 'antd';
-import { saveInfo, updateInfo } from './api';
-const FormItem = Form.Item;
+import React, { Component } from 'react'
+import { Input, Form, Modal, InputNumber, message } from 'antd'
+import { saveInfo, updateInfo } from './api'
+const FormItem = Form.Item
 
 const formItemLayout = {
   labelCol: {
-    sm: { span: 4 },
+    sm: { span: 4 }
   },
   wrapperCol: {
-    sm: { span: 16 },
-  },
-};
+    sm: { span: 16 }
+  }
+}
 
 class Edit extends Component {
 
@@ -19,10 +19,10 @@ class Edit extends Component {
     sort: ''
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps (nextProps) {
     if (this.id != nextProps.data.id) {
-      this.id = nextProps.data.id;
-      this.data = nextProps.data;
+      this.id = nextProps.data.id
+      this.data = nextProps.data
       this.setState({
         name: nextProps.data.name,
         sort: nextProps.data.sort
@@ -39,63 +39,66 @@ class Edit extends Component {
   // }
 
   onCancel = () => {
-    this.props.close();
+    this.props.close()
   }
 
   onOk = () => {
     const {
       form: { validateFields }
-    } = this.props;
+    } = this.props
     validateFields((err, vals) => {
       if (!err) {
         const params = {
-          ...vals,
-        };
-        if (this.id) params.id = this.id;
+          ...vals
+        }
+        if (this.id) {
+          params.id = this.id
+        }
         (this.id ? updateInfo : saveInfo)(params).then(data => {
           if (data && data.id) {
-            message.success('保存成功');
-            this.props.close('reload');
+            message.success('保存成功')
+            this.props.close('reload')
           }
-        });
+        })
       }
-    });
+    })
   }
 
-  render() {
+  render () {
     const {
-      form: { getFieldDecorator },
-    } = this.props;
+      form: { getFieldDecorator }
+    } = this.props
 
     return (
       <Modal
+        title={this.props.title}
         visible={this.props.visible}
         onCancel={this.onCancel}
         onOk={this.onOk}
         destroyOnClose
       >
         <Form {...formItemLayout}>
-          <FormItem label="热词名称">
+          <FormItem label='热词名称'>
             {getFieldDecorator('name', {
               initialValue: this.state.name,
               rules: [
                 {
                   required: true,
-                  message: '请输入热词名称',
-                },
+                  message: '请输入热词名称'
+                }
               ]
             })(<Input />)}
           </FormItem>
-          <FormItem label="排序">
+          <FormItem label='排序'>
             {getFieldDecorator('sort', {
               initialValue: this.state.sort,
               rules: [
                 {
                   required: true,
-                  message: '请输入排序',
-                },
+                  message: '请输入排序'
+                }
               ]
-            })(<Input type="number" />)}
+            })(<Input type='number' />)}
           </FormItem>
         </Form>
       </Modal>
@@ -103,4 +106,4 @@ class Edit extends Component {
   }
 }
 
-export default Form.create()(Edit);
+export default Form.create()(Edit)
