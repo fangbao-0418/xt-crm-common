9f0b348ad1c8160b66459c3db13b1080bf5cee74
biansaijun <biansaijun@hzxituan.com>
2020-05-25 12:00:47 +0800

    feat：0元购奖励提示优化

--- src/pages/activity/add.js
+++ src/pages/activity/add.js
@@ -302,21 +302,25 @@ class ActivityForm extends React.Component {
                   rules: [
                     {
                       required: type === 13,
-                      validator: (_, value, callback) => {
-                        if (type !== 13) {
-                          callback()
-                          return
-                        }
-                        if (value > 10 || value <= 0 || value === undefined) {
-                          callback('请输入大于0且小于等于10的整数或两位小数')
-                        } else {
-                          callback()
-                        }
-                      }
+                      message: '请输入奖励金额'
                     }
+                    // {
+                    //   required: type === 13,
+                    //   validator: (_, value, callback) => {
+                    //     if (type !== 13) {
+                    //       callback()
+                    //       return
+                    //     }
+                    //     if (value > 10 || value <= 0 || value === undefined) {
+                    //       callback('请输入大于0且小于等于10的整数或两位小数')
+                    //     } else {
+                    //       callback()
+                    //     }
+                    //   }
+                    // }
                   ]
                 })(
-                  <InputNumber precision={2} placeholder='请输入' />
+                  <InputNumber max={10} min={0.01} precision={2} placeholder='请输入' />
                 )}
                 <span> 元</span>
               </FormItem>
