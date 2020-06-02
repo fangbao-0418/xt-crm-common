62b1fe8eaab72458d44132c4a3752a9e5780bc1b
biansaijun <biansaijun@hzxituan.com>
2020-06-02 16:33:02 +0800

    feat: 生鲜订单申请售后条件修改显示条件

--- src/pages/fresh/order/detail/goods-table.js
+++ src/pages/fresh/order/detail/goods-table.js
@@ -24,7 +24,7 @@ class GoodsTable extends Component {
    * orderStatus 订单状态（10：待付款；20：待发货；30：已发货；50完成）
    */
   showApplyBtn = (orderStatus) => {
-    return orderStatus > 10 && orderStatus < 60
+    return orderStatus >= 30 && orderStatus < 60
   }
 
   handleApply = (record) => {
