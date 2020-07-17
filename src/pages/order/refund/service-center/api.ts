38cb2cb9d235f0f5cebf15857e31f7f870279ce8
cuizhu <jiguang.hjg@dtwave-inc.com>
2020-07-17 11:48:54 +0800

    feat: 请求地址加时间戳

--- src/pages/order/refund/service-center/api.ts
+++ src/pages/order/refund/service-center/api.ts
@@ -1,7 +1,7 @@
 export const fetchQuestion = () => {
   const env = process.env.PUB_ENV
 
-  const path = `https://sh-tximg.hzxituan.com/question/question-${env}.json`
+  const path = `https://sh-tximg.hzxituan.com/question/question-${env}.json?v=${new Date().getTime()}`
   return fetch(path).then((res) => {
     return res.json()
   })
