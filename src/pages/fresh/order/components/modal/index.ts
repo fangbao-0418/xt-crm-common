37a69501d3de758d786a1631aa49581d90fb844b
fangbao <fangbao@hzxituan.com>
2020-08-06 19:54:16 +0800

    feat: 删除无用代码

--- src/pages/fresh/order/components/modal/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as RemarkModal} from './remark-modal';
-export {default as ApplyAfterSale} from './ApplyAfterSale';
-export {default as ModifyAddress} from './ModifyShippingAddress';
--- src/routes/modules.ts
+++ src/routes/modules.ts
@@ -1,20 +1,10 @@
-/*
- * @Author: fangbao
- * @Date: 2020-05-19 23:09:49
- * @LastEditors: fangbao
- * @LastEditTime: 2020-06-05 21:48:15
- * @FilePath: /xt-crm-microservice/common/src/routes/modules.ts
- */
 import Loadable from '@/util/loadable'
-
 export { view as Login } from '@/pages/login'
 export const Home = Loadable(() => import('@/pages/home'))
 export const Settings = Loadable(() => import('@/pages/settings'))
 export const Goods = Loadable(() => import('@/pages/goods'))
 export const Template = Loadable(() => import('@/pages/template'))
 export const Order = Loadable(() => import('@/pages/order'))
-// export const Activity = Loadable(() => import('@/pages/activity'))
-// export const Coupon = Loadable(() => import('@/pages/coupon'))
 export const User = Loadable(() => import('@/pages/user'))
 export const Supplier = Loadable(() => import('@/pages/supplier'))
 export const Banner = Loadable(() => import('@/pages/banner'))
@@ -22,10 +12,7 @@ export const Finance = Loadable(() => import('@/pages/finance'))
 export const Auth = Loadable(() => import('@/pages/auth'))
 export const Interface = Loadable(() => import('@/pages/interface'))
 export const CrudPage = Loadable(() => import('@/components/crudPage'))
-// export const Message = Loadable(() => import('@/pages/message'))
 export const Setting = Loadable(() => import('@/pages/setting'))
-// export const ULive = Loadable(() => import('@/pages/ulive'))
 export const MerchantAccounts = Loadable(() => import('@/pages/merchant-accounts'))
 export const Shop = Loadable(() => import('@/pages/shop'))
-export const Fresh = Loadable(() => import('@/pages/fresh'))
 export const System = Loadable(() => import('@/pages/system'))
