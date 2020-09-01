9a2df6356d1bdb1781627a5a8648a1cb35e4f524
biansaijun <biansaijun@hzxituan.com>
2020-09-01 14:40:50 +0800

    feat： 供应商条件查询优化

--- src/pages/order/refund/config.tsx
+++ src/pages/order/refund/config.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import GoodCell from '@/components/good-cell'
 import SuppilerSelect from '@/components/suppiler-auto-select'
+import SearchFetch from '@/packages/common/components/search-fetch'
 import refundType from '@/enum/refundType'
 import createType from '@/enum/createType'
 import { Button } from 'antd'
@@ -8,7 +9,8 @@ import MoneyRender from '@/components/money-render'
 import { formatDate } from '@/pages/helper'
 import SelectFetch from '@/components/select-fetch'
 import {
-  getShopTypes
+  getShopTypes,
+  supplierSearch
 } from '../api'
 export const namespace = 'refundOrder'
 
@@ -98,9 +100,27 @@ export const formFields = function (
     },
     {
       type: 'input',
-      id: 'storeId',
+      id: 'store',
       label: '供应商',
-      render: () => <SuppilerSelect />
+      render: () => {
+        return (
+          <SearchFetch
+            selectProps={{
+              labelInValue: true
+            }}
+            api={(name) => {
+              return supplierSearch(name).then((res: any) => {
+                return res.map((item: any) => ({
+                  text: item.name,
+                  value: item.id
+                }))
+              })
+            }}
+            style={{ width: 172 }}
+            placeholder='请输入店铺名称'
+          />
+        )
+      }
     },
     {
       type: 'input',
