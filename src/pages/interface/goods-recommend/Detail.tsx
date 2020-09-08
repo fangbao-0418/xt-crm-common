5269888f421445b5456708d16b4aa35228acd639
wucl <1445860318@qq.com>
2020-09-08 14:57:49 +0800

    feat: 我的推荐

--- src/pages/interface/goods-recommend/Detail.tsx
+++ src/pages/interface/goods-recommend/Detail.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Button, Radio } from 'antd'
 import Form, { FormItem, FormInstance } from '@/packages/common/components/form'
 import { getFieldsConfig, locationMap, displayFromMap } from './config'
 import RelevanceGoods from './components/RelevanceGoods'
+import RelevanceShop from './components/RelevanceShop'
 import { withRouter, RouteComponentProps } from 'react-router'
 import * as api from './api'
 interface Props extends RouteComponentProps<{id: string}> {
@@ -89,7 +90,7 @@ class Main extends React.Component<Props, State> {
             name='displayFrom'
             verifiable
           />
-          <FormItem
+          {/* <FormItem
             label='关联商品'
             required
             // name='productRecommendSpuList'
@@ -110,6 +111,69 @@ class Main extends React.Component<Props, State> {
                 }
               )(
                 <RelevanceGoods readonly={readonly} />
+              )
+            }}
+          /> */}
+          <FormItem
+            label='推荐内容'
+            required
+            inner={(form) => {
+              return form.getFieldDecorator('relationType', {
+                initialValue: 10
+              })(
+                <Radio.Group>
+                  <Radio value={10}>关联商品</Radio>
+                  <Radio value={20}>关联店铺</Radio>
+                </Radio.Group>
+              )
+            }}
+          />
+          <FormItem
+            labelCol={{ span: 0 }}
+            wrapperCol={{ span: 24 }}
+            style={{ marginBottom: 0 }}
+            inner={(form) => {
+              const relationType = form.getFieldValue('relationType')
+              return relationType === 10 ? (
+                <FormItem
+                  inner={(form) => {
+                    return form.getFieldDecorator('productRecommendSpuList', {
+                      rules: [
+                        {
+                          validator: (rule, value, cb) => {
+                            if (!value || value.length === 0) {
+                              cb('关联商品不能为空')
+                            } else {
+                              cb()
+                            }
+                          }
+                        }
+                      ]
+                    })(
+                      <RelevanceGoods readonly={readonly} />
+                    )
+                  }}
+                />
+              ) : (
+                <FormItem
+                  inner={(form) => {
+                    return form.getFieldDecorator('relationIdList', {
+                      rules: [
+                        {
+                          validator: (rule, value, cb) => {
+                            if (!value || value.length === 0) {
+                              cb('关联店铺不能为空')
+                            } else {
+                              cb()
+                            }
+                          }
+                        }
+                      ]
+                    })(
+                      <RelevanceShop readonly={readonly} />
+                    )
+                  }}
+                />
               )
             }}
           />
---
+++ src/pages/interface/goods-recommend/components/RelevanceShop.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Table } from "antd";
+import { ColumnProps } from "antd/es/table";
+
+interface Props {
+  readonly: boolean
+}
+interface State {
+  dataSource: any[]
+}
+class Main extends React.Component<Props, State> {
+  public state = {
+    dataSource: []
+  }
+  public columns: ColumnProps<any>[] = [
+    {
+      title: "店铺ID",
+      dataIndex: "shopId",
+    },
+    {
+      title: "店铺名称",
+      dataIndex: "shopName",
+    },
+    {
+      title: "在架商品",
+      dataIndex: "product",
+    },
+    {
+      title: "操作",
+      render: (records) => {
+        return <span className="href">删除</span>;
+      },
+    },
+  ];
+  public render() {
+    const { readonly } = this.props
+    return (
+      <div>
+        <div>
+          <Table
+            style={{
+              width: 600,
+            }}
+            columns={this.columns}
+          />
+        </div>
+        <div>
+          {!readonly && (
+            <span
+              className="href"
+              onClick={() => {
+                const ref: any = this.refs.shopmodal;
+                ref.open(this.state.dataSource);
+              }}
+            >
+              +添加店铺
+            </span>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
