1c88d96109ee0ff7ebf044de18a13ff373e688ef
wucl <1445860318@qq.com>
2020-09-19 15:18:07 +0800

    feat: 过滤赛选条件

--- src/pages/interface/category/components/ActicityModal.tsx
+++ src/pages/interface/category/components/ActicityModal.tsx
@@ -30,26 +30,20 @@ const formConfig: FieldsConfig = {
         label: '全部',
         value: 0
       }, {
-        label: '待发布',
-        value: 1
-      }, {
         label: '已发布',
         value: 2
       }, {
         label: '报名中',
         value: 3
       }, {
+        label: '未开始',
+        value: 8
+      }, {
         label: '预热中',
         value: 4
       }, {
         label: '进行中',
         value: 5
-      }, {
-        label: '已结束',
-        value: 6
-      }, {
-        label: '已关闭',
-        value: 7
       }]
     },
     effectiveTime: {
