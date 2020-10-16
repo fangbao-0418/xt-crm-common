d18b58ea27d5bc84aa00a39cf9d30c0215d77527
wucl <1445860318@qq.com>
2020-10-16 14:25:05 +0800

    feat: icon管理

--- src/pages/haodian-business-school/icon/api.ts
+++ src/pages/haodian-business-school/icon/api.ts
@@ -1,21 +1,22 @@
 const { post, get } = APP.http
 
+// 业务源:0-优选，10-买菜，20-好店,21-优选商学院，22-好店商学院
 export function getIconList () {
-  return get('/homeicon/list')
+  return get('/mcweb/homeicon/list?bizSource=22')
 }
 
 export function saveIcon (payload: HomeIcon.ItemProps) {
-  return post('/homeicon/update', payload)
+  return post('/mcweb/homeicon/update', payload)
 }
 
 export function addIcon (payload: HomeIcon.ItemProps) {
-  return post('/homeicon/add', payload)
+  return post('/mcweb/homeicon/add?bizSource=22', payload)
 }
 
 export function deleteIcon (id: any) {
-  return post(`/homeicon/delete?id=${id}`)
+  return post(`/mcweb/homeicon/delete?id=${id}`)
 }
 
 export function publishIcon () {
-  return post('/homeicon/publish')
+  return post('/mcweb/homeicon/publish')
 }
