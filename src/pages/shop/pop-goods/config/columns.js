7dc2513f31ad16fc9054f680a55ff3e5eb88cc6f
fangbao <fangbao@hzxituan.com>
2020-11-07 17:08:51 +0800

    feat: 去掉多余违规次数

--- src/pages/shop/pop-goods/config/columns.js
+++ src/pages/shop/pop-goods/config/columns.js
@@ -162,17 +162,6 @@ const getColumns = ({ onPreview, onViolation, onDetail, onLower, onPass, onUnpas
     {
       title: '违规次数',
       dataIndex: 'violationCount',
-      width: 200,
-      render: (val, record) => {
-        if (val === 0) {
-          return val
-        }
-        return <span onClick={() => onViolation(record)} className='href'>{val}</span>
-      }
-    },
-    {
-      title: '违规次数',
-      dataIndex: 'violationCount',
       width: 100,
       align: 'center'
     },
